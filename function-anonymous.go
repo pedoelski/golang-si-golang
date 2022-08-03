8448550ca9d2b4bd4b4a174d6cd85a47b17f1fdc
ar <asep@assemblrworld.com>
2022-08-04 04:15:03 +0700

    add variadic, mulitple value, as params, anonim, closure, break-cont

---
+++ function-anonymous.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type Blacklist func(string) bool
+
+func registerUser(name string, ) {
+
+}
+
+func main() {
+	fun := func() string {
+		return "hello World"
+	}
+
+	fmt.Println(fun)
+}
